1bb67f6d7988b880483f3791bc1d7c04a5a2fc16
Shota Nozaki <emonkak@gmail.com>
2015-11-07 17:32:35 +0900

    Refactor di

--- src/js/di/annotations.ts
+++ src/js/di/annotations.ts
@@ -3,21 +3,20 @@
 import 'reflect-metadata'
 import { IInjectableKey } from './interfaces'
 import { singletonScope, prototypeScope } from './scopes'
-
-function merge<T>(xs: T[], ys: T[]): T[] {
-    const zs = [].concat(xs)
-    ys.forEach((y, i) => {
-        zs[i] = y
-    })
-    return zs
-}
+import { merge } from './utils'
 
 export function Inject(target: any, key?: any, desc?: any): any {
     if (typeof key !== 'undefined') {
-        target[key].$inject = merge(Reflect.getMetadata('design:paramtypes', target, key), target[key].$inject || [])
+        target[key].$inject = merge(
+            Reflect.getMetadata('design:paramtypes', target, key),
+            target[key].$inject || []
+        )
         return target[key]
     } else {
-        target.$inject = merge(Reflect.getMetadata('design:paramtypes', target), target.$inject || [])
+        target.$inject = merge(
+            Reflect.getMetadata('design:paramtypes', target),
+            target.$inject || []
+        )
         return target
     }
 }
