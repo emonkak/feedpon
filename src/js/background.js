3f6b8816d4d4fb728feba1afc152fe68be0e1d86
Shota Nozaki <emonkak@gmail.com>
2016-01-19 01:57:44 +0900

    Refactor background.js

--- src/js/background.js
+++ src/js/background.js
@@ -1,6 +1,7 @@
 import 'regenerator/runtime'
 
 import ActionDispatcher from './shared/dispatchers/ActionDispatcher'
+import ExpandUrlHandler from './handlers/ExpandUrlHandler'
 import FetchCategoriesHandler from './handlers/FetchCategoriesHandler'
 import FetchContentsHandler from './handlers/FetchContentsHandler'
 import FetchFullContentHandler from './handlers/FetchFullContentHandler'
@@ -10,9 +11,14 @@ import GetCategoriesCacheHandler from './handlers/GetCategoriesCacheHandler'
 import GetCredentialHandler from './handlers/GetCredentialHandler'
 import GetSubscriptionsCacheHandler from './handlers/GetSubscriptionsCacheHandler'
 import GetUnreadCountsCacheHandler from './handlers/GetUnreadCountsCacheHandler'
-import ExpandUrlHandler from './handlers/ExpandUrlHandler'
 import container from './container'
 import { ExpandUrl, FetchCategories, FetchContents, FetchFullContent, FetchSubscriptions, FetchUnreadCounts, GetCategoriesCache, GetCredential, GetSubscriptionsCache, GetUnreadCountsCache } from './constants/actionTypes'
+import { FromEventPatternObservable } from 'rxjs/observable/fromEventPattern'
+import { Observable } from 'rxjs/Observable'
+import { Subscription } from 'rxjs/Subscription'
+import { filter } from 'rxjs/operator/filter'
+import { map } from 'rxjs/operator/map'
+import { mergeMap } from 'rxjs/operator/mergeMap'
 
 const actionDispatcher = new ActionDispatcher(container)
     .mount(ExpandUrl, ExpandUrlHandler)
@@ -26,27 +32,46 @@ const actionDispatcher = new ActionDispatcher(container)
     .mount(GetSubscriptionsCache, GetSubscriptionsCacheHandler)
     .mount(GetUnreadCountsCache, GetUnreadCountsCacheHandler)
 
-function handleConnect(port) {
-    chrome.runtime.onMessage.addListener(handleMessage)
-    port.onDisconnect.addListener(handleDisconnect)
+const ports = FromEventPatternObservable.create(
+    ::chrome.runtime.onConnect.addListener,
+    ::chrome.runtime.onConnect.removeListener
+)
 
-    function handleMessage(request, sender, sendResponse) {
-        actionDispatcher.dispatch(request)
+const messages = FromEventPatternObservable.create(
+    ::chrome.runtime.onMessage.addListener,
+    ::chrome.runtime.onMessage.removeListener,
+    (request, sender, sendResponse) => ({ request, sender, sendResponse })
+)
+
+const portsAndMessages = ports
+    ::mergeMap(port => Observable.create(subscriber => {
+        const subscription = FromEventPatternObservable.create(
+                ::port.onDisconnect.addListener,
+                ::port.onDisconnect.removeListener
+            )
+            .subscribe(() => subscriber.complete())
+        subscriber.add(subscription)
+
+        messages
+            ::filter(message => message.sender.tab.id === port.sender.tab.id)
+            ::map(message => ({ port, message }))
+            .subscribe(subscriber)
+
+        subscriber.add(new Subscription(() => {
+            port.disconnect()
+        }))
+    }))
+
+portsAndMessages
+    .subscribe(({ port, message }) => {
+        actionDispatcher.dispatch(message.request)
             .subscribe(
                 event => port.postMessage(event),
-                error => sendResponse({ error: error ? error.toString() : null }),
-                () => sendResponse({})
+                error => message.sendResponse({ error: error ? error.toString() : null }),
+                () => message.sendResponse({})
             )
-        return true
-    }
+    })
 
-    function handleDisconnect(port) {
-        chrome.runtime.onMessage.removeListener(handleMessage)
-        port.onDisconnect.removeListener(handleDisconnect)
-    }
-}
-
-chrome.runtime.onConnect.addListener(handleConnect)
 chrome.browserAction.onClicked.addListener(() => {
     chrome.tabs.create({ url: 'index.html' })
 })
