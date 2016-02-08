116def6e8892c0bc648db7dbdc8d124893d15606
Shota Nozaki <emonkak@gmail.com>
2016-02-09 02:21:59 +0900

    To safety action and event types

--- src/js/constants/actionTypes.ts
+++ src/js/constants/actionTypes.ts
@@ -1,81 +1,81 @@
 import * as feedly from '../services/feedly/interfaces'
 import { Action, Event } from '../shared/interfaces'
 
-export const Authenticate = 'Authenticate'
-export interface Authenticate extends Action<string> {
+export const Authenticate: 'Authenticate' = 'Authenticate'
+export interface Authenticate extends Action<typeof Authenticate> {
 }
 
-export const DispatchEvent = 'DispatchEvent'
-export interface DispatchEvent<TEvent extends Event<string>> extends Action<string> {
+export const DispatchEvent: 'DispatchEvent' = 'DispatchEvent'
+export interface DispatchEvent<TEvent extends Event<string>> extends Action<typeof DispatchEvent> {
     event: TEvent
 }
 
-export const ExpandUrl = 'ExpandUrl'
-export interface ExpandUrl extends Action<string> {
+export const ExpandUrl: 'ExpandUrl' = 'ExpandUrl'
+export interface ExpandUrl extends Action<typeof ExpandUrl> {
     url: string
 }
 
-export const FetchContents = 'FetchContents'
-export interface FetchContents extends Action<string> {
+export const FetchContents: 'FetchContents' = 'FetchContents'
+export interface FetchContents extends Action<typeof FetchContents> {
     payload: feedly.GetStreamInput
 }
 
-export const FetchCategories = 'FetchCategories'
-export interface FetchCategories extends Action<string> {
+export const FetchCategories: 'FetchCategories' = 'FetchCategories'
+export interface FetchCategories extends Action<typeof FetchCategories> {
 }
 
-export const FetchFullContent = 'FetchFullContent'
-export interface FetchFullContent extends Action<string> {
+export const FetchFullContent: 'FetchFullContent' = 'FetchFullContent'
+export interface FetchFullContent extends Action<typeof FetchFullContent> {
     streamId: string
     url: string
 }
 
-export const FetchSubscriptions = 'FetchSubscriptions'
-export interface FetchSubscriptions extends Action<string> {
+export const FetchSubscriptions: 'FetchSubscriptions' = 'FetchSubscriptions'
+export interface FetchSubscriptions extends Action<typeof FetchSubscriptions> {
 }
 
-export const FetchUnreadCounts = 'FetchUnreadCounts'
-export interface FetchUnreadCounts extends Action<string> {
+export const FetchUnreadCounts: 'FetchUnreadCounts' = 'FetchUnreadCounts'
+export interface FetchUnreadCounts extends Action<typeof FetchUnreadCounts> {
     payload: feedly.GetUnreadCountsInput
 }
 
-export const GetCategoriesCache = 'GetCategoriesCache'
-export interface GetCategoriesCache extends Action<string> {
+export const GetCategoriesCache: 'GetCategoriesCache' = 'GetCategoriesCache'
+export interface GetCategoriesCache extends Action<typeof GetCategoriesCache> {
 }
 
-export const GetCredential = 'GetCredential'
-export interface GetCredential extends Action<string> {
+export const GetCredential: 'GetCredential' = 'GetCredential'
+export interface GetCredential extends Action<typeof GetCredential> {
 }
 
-export const GetSubscriptionsCache = 'GetSubscriptionsCache'
-export interface GetSubscriptionsCache extends Action<string> {
+export const GetSubscriptionsCache: 'GetSubscriptionsCache' = 'GetSubscriptionsCache'
+export interface GetSubscriptionsCache extends Action<typeof GetSubscriptionsCache> {
 }
 
-export const GetUnreadCountsCache = 'GetUnreadCountsCache'
-export interface GetUnreadCountsCache extends Action<string> {
+export const GetUnreadCountsCache: 'GetUnreadCountsCache' = 'GetUnreadCountsCache'
+export interface GetUnreadCountsCache extends Action<typeof GetUnreadCountsCache> {
 }
 
 export namespace History {
-    export const Push = 'History/Push'
-    export interface Push extends Action<string> {
+    export const Push: 'History/Push' = 'History/Push'
+    export interface Push extends Action<typeof Push> {
         path: string
     }
 
-    export const Replace = 'History/Replace'
-    export interface Replace extends Action<string> {
+    export const Replace: 'History/Replace' = 'History/Replace'
+    export interface Replace extends Action<typeof Replace> {
         path: string
     }
 
-    export const Go = 'History/Go'
-    export interface Go extends Action<string> {
+    export const Go: 'History/Go' = 'History/Go'
+    export interface Go extends Action<typeof Go> {
         n: number
     }
 
-    export const GoBack = 'History/GoBack'
-    export interface GoBack extends Action<string> {
+    export const GoBack: 'History/GoBack' = 'History/GoBack'
+    export interface GoBack extends Action<typeof GoBack> {
     }
 
-    export const GoForward = 'History/GoForward'
-    export interface GoForward extends Action<string> {
+    export const GoForward: 'History/GoForward' = 'History/GoForward'
+    export interface GoForward extends Action<typeof GoForward> {
     }
 }
--- src/js/constants/eventTypes.ts
+++ src/js/constants/eventTypes.ts
@@ -1,39 +1,39 @@
 import * as feedly from '../services/feedly/interfaces'
 import { Event, Action } from '../shared/interfaces'
 
-export const ActionDone = 'ActionDone'
-export interface ActionDone extends Event<string> {
+export const ActionDone: 'ActionDone' = 'ActionDone'
+export interface ActionDone extends Event<typeof ActionDone> {
     action: Action<any>
 }
 
-export const ActionFailed = 'ActionFailed'
-export interface ActionFailed extends Event<string> {
+export const ActionFailed: 'ActionFailed' = 'ActionFailed'
+export interface ActionFailed extends Event<typeof ActionFailed> {
     action: Action<any>
     error: any
 }
 
-export const CategoriesReceived = 'CategoriesReceived'
-export interface CategoriesReceived extends Event<string> {
+export const CategoriesReceived: 'CategoriesReceived' = 'CategoriesReceived'
+export interface CategoriesReceived extends Event<typeof CategoriesReceived> {
     categories: feedly.Category[]
 }
 
-export const ContentsReceived = 'ContentsReceived'
-export interface ContentsReceived extends Event<string> {
+export const ContentsReceived: 'ContentsReceived' = 'ContentsReceived'
+export interface ContentsReceived extends Event<typeof ContentsReceived> {
     contents: feedly.Contents
 }
 
-export const CredentialReceived = 'CredentialReceived'
-export interface CredentialReceived extends Event<string> {
+export const CredentialReceived: 'CredentialReceived' = 'CredentialReceived'
+export interface CredentialReceived extends Event<typeof CredentialReceived> {
     credential: feedly.Credential
 }
 
-export const EntryActivated = 'EntryActivated'
-export interface EntryActivated extends Event<string> {
+export const EntryActivated: 'EntryActivated' = 'EntryActivated'
+export interface EntryActivated extends Event<typeof EntryActivated> {
     entry: feedly.Entry
 }
 
-export const FullContentReceived = 'FullContentReceived'
-export interface FullContentReceived extends Event<string> {
+export const FullContentReceived: 'FullContentReceived' = 'FullContentReceived'
+export interface FullContentReceived extends Event<typeof FullContentReceived> {
     fullContent: FullContent
 }
 interface FullContent {
@@ -43,23 +43,23 @@ interface FullContent {
     nextLink?: string
 }
 
-export const LocationUpdated = 'LocationUpdated'
-export interface LocationUpdated extends Event<string> {
+export const LocationUpdated: 'LocationUpdated' = 'LocationUpdated'
+export interface LocationUpdated extends Event<typeof LocationUpdated> {
     location: HistoryModule.Location
 }
 
-export const SubscriptionsReceived = 'SubscriptionsReceived'
-export interface SubscriptionsReceived extends Event<string> {
+export const SubscriptionsReceived: 'SubscriptionsReceived' = 'SubscriptionsReceived'
+export interface SubscriptionsReceived extends Event<typeof SubscriptionsReceived> {
     subscriptions: feedly.Subscription[]
 }
 
-export const UnreadCountsReceived = 'UnreadCountsReceived'
-export interface UnreadCountsReceived extends Event<string> {
+export const UnreadCountsReceived: 'UnreadCountsReceived' = 'UnreadCountsReceived'
+export interface UnreadCountsReceived extends Event<typeof UnreadCountsReceived> {
     unreadCounts: feedly.UnreadCount[]
 }
 
-export const UrlExpanded = 'UrlExpanded'
-export interface UrlExpanded extends Event<string> {
+export const UrlExpanded: 'UrlExpanded' = 'UrlExpanded'
+export interface UrlExpanded extends Event<typeof UrlExpanded> {
     url: string
     redirectUrl: string
 }
