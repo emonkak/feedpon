53a954997f2490b7ee2f0e529fbae3f578b1291c
Shota Nozaki <emonkak@gmail.com>
2017-02-22 01:58:18 +0900

    Update packages

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    plugins: [
+        require('postcss-import')(),
+        require('postcss-cssnext')({
+            "browsers": "last 2 versions",
+            "warnForDuplicates": false,
+        })
+    ]
+};
