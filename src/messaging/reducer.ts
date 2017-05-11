6e979f31a0aa47f772580748004e6b71832624ef
Shota Nozaki <emonkak@gmail.com>
2017-05-11 21:44:31 +0900

    Add combineReducer() function

--- src/messaging/reducer.ts
+++ src/messaging/reducer.ts
@@ -1,5 +1,6 @@
 import { State, SyncEvent } from 'messaging/types';
 
+import combineReducers from 'utils/combineReducers';
 import credentialReducer from 'messaging/credential/reducer';
 import notificationsReducer from 'messaging/notification/reducer';
 import searchReducer from 'messaging/search/reducer';
@@ -7,15 +8,13 @@ import siteinfoReducer from 'messaging/siteinfo/reducer';
 import streamReducer from 'messaging/stream/reducer';
 import subscriptionsReducer from 'messaging/subscription/reducer';
 
-export default function reducer(state: State, event: SyncEvent): State {
-    return {
-        credential: credentialReducer(state.credential, event),
-        environment: state.environment,
-        notifications: notificationsReducer(state.notifications, event),
-        search: searchReducer(state.search, event),
-        settings: state.settings,
-        siteinfo: siteinfoReducer(state.siteinfo, event),
-        stream: streamReducer(state.stream, event),
-        subscriptions: subscriptionsReducer(state.subscriptions, event)
-    };
-}
+export default combineReducers<State, SyncEvent>({
+    credential: credentialReducer,
+    environment: (state, event) => state,
+    notifications: notificationsReducer,
+    search: searchReducer,
+    settings: (state, event) => state,
+    siteinfo: siteinfoReducer,
+    stream: streamReducer,
+    subscriptions: subscriptionsReducer
+});
