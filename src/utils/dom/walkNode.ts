9bebe73209e384770b5dd420cce47d8bf311ed62
Shota Nozaki <emonkak@gmail.com>
2017-05-14 19:24:29 +0900

    Fix walkNode()

--- src/utils/dom/walkNode.ts
+++ src/utils/dom/walkNode.ts
@@ -4,18 +4,23 @@ export default function walkNode(node: Node, callback: (node: Node) => boolean):
     let currentNode: Node | null = node;
 
     do {
-        // Save in advance
+        // Save the parent-child relationship in advance because it may be deleted
         const { firstChild, nextSibling, parentNode }: {
             firstChild: Node | null,
             nextSibling: Node | null,
             parentNode: Node | null
         } = currentNode;
 
-        if (walk && (currentNode = firstChild)) {
+        if (walk && depth > 0) {
             walk = callback(currentNode)
+        }
+
+        if (walk && firstChild) {
+            currentNode = firstChild;
             depth++;
-        } else if (currentNode = nextSibling) {
-            walk = callback(currentNode)
+        } else if (nextSibling) {
+            currentNode = nextSibling;
+            walk = true;
         } else {
             currentNode = parentNode;
             walk = false;
