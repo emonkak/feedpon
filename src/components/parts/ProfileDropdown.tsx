1b3c506fd0cbc8f3b77d74a0b208ac348a9e90f9
Shota Nozaki <emonkak@gmail.com>
2017-07-28 12:07:00 +0900

    Foece wrapping on <ProfileDropdown>

--- src/components/parts/ProfileDropdown.tsx
+++ src/components/parts/ProfileDropdown.tsx
@@ -69,8 +69,8 @@ export default class ProfileDropdown extends PureComponent<ProfileDropdownProps,
                             <div className="u-flex u-flex-align-items-center dropdown-arrow">
                                 {icon}
                                 <span className="u-flex-grow-1 u-margin-left-1 u-text-left">
-                                    <div><strong>{profile.userId}</strong></div>
-                                    <div className="u-text-small">via <strong>{profile.source}</strong></div>
+                                    <div className="u-text-wrap"><strong>{profile.userId}</strong></div>
+                                    <div className="u-text-wrap u-text-small">via <strong>{profile.source}</strong></div>
                                 </span>
                             </div>
                         </button>
