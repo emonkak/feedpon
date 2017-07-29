acb6efd17b9f27d6f7160e413cc20a32391be8c4
Shota Nozaki <emonkak@gmail.com>
2017-07-29 12:13:43 +0900

    Implement chunked request on hatena bookmark api

--- src/adapters/hatena/bookmarkApi.ts
+++ src/adapters/hatena/bookmarkApi.ts
@@ -1,13 +1,25 @@
 import * as types from './types';
 import buildQueryString from 'utils/buildQueryString';
 
-export function getBookmarkCounts(urls: string[]): Promise<types.BookmarkCounts> {
-    const requestUrl =  'http://api.b.st-hatena.com/entry.counts?'
-        + buildQueryString({ url: urls });
+const URL_CHUNK_LIMIT = 50;
+const URL_LRNGTH_LIMIT = 4096;
 
-    return fetch(requestUrl)
-        .then<Response>((response) => response.ok ? response : Promise.reject(response))
-        .then<types.BookmarkCounts>((response) => response.json());
+export function getBookmarkCounts(urls: string[]): Promise<types.BookmarkCounts> {
+    const promises = [];
+
+    for (const chunkedUrls of chunkUrls(urls)) {
+        const requestUrl =  'http://api.b.st-hatena.com/entry.counts?'
+            + buildQueryString({ url: chunkedUrls });
+
+        const promise = fetch(requestUrl)
+            .then<Response>((response) => response.ok ? response : Promise.reject(response.url + ': ' + response.statusText))
+            .then<types.BookmarkCounts>((response) => response.json());
+
+        promises.push(promise);
+    }
+
+    return Promise.all(promises)
+        .then((results) => Object.assign({}, ...results));
 }
 
 export function getBookmarkEntry(url: string): Promise<types.GetEntryResponse | null> {
@@ -15,6 +27,33 @@ export function getBookmarkEntry(url: string): Promise<types.GetEntryResponse | 
         + buildQueryString({ url });
 
     return fetch(requestUrl)
-        .then<Response>((response) => response.ok ? response : Promise.reject(response))
+        .then<Response>((response) => response.ok ? response : Promise.reject(response.url + ': ' + response.statusText))
         .then<types.GetEntryResponse>((response) => response.json());
+}
+
+function chunkUrls(urls: string[]): string[][] {
+    if (urls.length === 0) {
+        return [];
+    }
+
+    let currentChunk: string[] = [];
+    let urlLength = 0;
+
+    const chunks = [currentChunk];
+
+    for (let i = 0, l = urls.length; i < l; i++) {
+        const url = urls[i];
+
+        if (currentChunk.length >= URL_CHUNK_LIMIT
+            || urlLength + url.length > URL_LRNGTH_LIMIT) {
+            currentChunk = [];
+            urlLength = 0;
+            chunks.push(currentChunk);
+        }
+
+        currentChunk.push(url);
+        urlLength += url.length;
+    }
+
+    return chunks;
 }
--- src/adapters/wedata/api.ts
+++ src/adapters/wedata/api.ts
@@ -6,7 +6,7 @@ export function getItems<T>(databaseName: string): Promise<types.WedataItem<T>[]
     const requestUrl = ENDPOINT + 'databases/' + databaseName + '/items_all.json';
 
     return fetch(requestUrl)
-        .then<Response>((response) => response.ok ? response : Promise.reject(response))
+        .then<Response>((response) => response.ok ? response : Promise.reject(response.url + ': ' + response.statusText))
         .then<types.WedataItem<T>[]>((response) => response.json());
 }
 
