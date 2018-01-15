c0c2cd771f7e79206254bfb5740d50a58e28521b
Shota Nozaki <emonkak@gmail.com>
2018-01-15 14:57:16 +0900

    Implement entries dropdown abbreviation display

--- src/components/parts/EntriesDropdown.tsx
+++ src/components/parts/EntriesDropdown.tsx
@@ -22,6 +22,8 @@ interface EntriesDropdownProps {
 
 interface EntriesDropdownState {
     isMarkingAllAsRead: boolean;
+    behindExpanded: boolean;
+    aheadExpanded: boolean;
 }
 
 export default class EntriesDropdown extends PureComponent<EntriesDropdownProps, EntriesDropdownState> {
@@ -29,44 +31,10 @@ export default class EntriesDropdown extends PureComponent<EntriesDropdownProps,
         super(props, context);
 
         this.state = { 
-            isMarkingAllAsRead: false
+            isMarkingAllAsRead: false,
+            behindExpanded: false,
+            aheadExpanded: false
         };
-
-        this.handleOpenMarkAllAsReadModal = this.handleOpenMarkAllAsReadModal.bind(this);
-        this.handleCloseMarkAllAsReadModal = this.handleCloseMarkAllAsReadModal.bind(this);
-    }
-
-    handleOpenMarkAllAsReadModal() {
-        this.setState({
-            isMarkingAllAsRead: true
-        });
-    }
-
-    handleCloseMarkAllAsReadModal() {
-        this.setState({
-            isMarkingAllAsRead: false
-        });
-    }
-
-    renderEntryMenuItem(entry: Entry, index: number) {
-        const { activeEntryIndex, onScrollToEntry, readEntryIndex } = this.props;
-
-        const icon = index == activeEntryIndex
-            ? <i className="icon icon-16 icon-dot u-text-negative" />
-            : entry.markedAsRead
-            ? <i className="icon icon-16 icon-dot u-text-muted" />
-            : index <= readEntryIndex
-            ? <i className="icon icon-16 icon-dot u-text-positive" />
-            : null;
-
-        return (
-            <MenuItem
-                icon={icon}
-                key={entry.entryId}
-                onSelect={onScrollToEntry}
-                primaryText={entry.title}
-                value={index} />
-        );
     }
 
     render() {
@@ -74,8 +42,9 @@ export default class EntriesDropdown extends PureComponent<EntriesDropdownProps,
             canMarkStreamAsRead,
             entries,
             keepUnread,
-            onClearReadEntries,
+            onClearReadPosition,
             onMarkStreamAsRead,
+            onScrollToEntry,
             onToggleUnreadKeeping,
             readEntryIndex,
             title
@@ -94,28 +63,35 @@ export default class EntriesDropdown extends PureComponent<EntriesDropdownProps,
                             {numCurrentReadEntries || ''}
                         </span>
                     </button>
-                }>
+                }
+                onClose={this._handleClose}>
                 <div className="menu-heading">Entries</div>
-                {entries.map(this.renderEntryMenuItem, this)}
+                {this._renderEntryMenuItems()}
+                <div className="menu-divider" />
+                <MenuItem
+                    isDisabled={readEntryIndex < 0}
+                    onSelect={onScrollToEntry}
+                    primaryText="Scroll to read position"
+                    value={readEntryIndex} />
+                <MenuItem
+                    isDisabled={numCurrentReadEntries === 0}
+                    onSelect={onClearReadPosition}
+                    primaryText="Clear read position" />
+                <div className="menu-divider" />
+                <MenuItem
+                    isDisabled={!canMarkStreamAsRead}
+                    onSelect={this._handleOpenMarkAllAsReadModal}
+                    primaryText="Mark all as read in stream..." />
                 <div className="menu-divider" />
                 <MenuItem
                     icon={keepUnread ? <i className="icon icon-16 icon-checkmark" /> : null}
                     onSelect={onToggleUnreadKeeping}
                     primaryText="Keep unread" />
-                <div className="menu-divider" />
-                <MenuItem
-                    isDisabled={numCurrentReadEntries === 0}
-                    onSelect={onClearReadEntries}
-                    primaryText="Clear read entries" />
-                <MenuItem
-                    isDisabled={!canMarkStreamAsRead}
-                    onSelect={this.handleOpenMarkAllAsReadModal}
-                    primaryText="Mark all as read in stream..." />
             </Dropdown>
             <Portal>
                 <ConfirmModal
                     isOpened={isMarkingAllAsRead}
-                    onClose={this.handleCloseMarkAllAsReadModal}
+                    onClose={this._handleCloseMarkAllAsReadModal}
                     message="Are you sure you want to mark as read in this stream?"
                     confirmButtonClassName="button button-positive"
                     confirmButtonLabel="Mark all as read"
@@ -123,5 +99,110 @@ export default class EntriesDropdown extends PureComponent<EntriesDropdownProps,
                     title={`Mark all as read in "${title}"`} />
             </Portal>
         </>;
+    }
+
+    private _renderEntryMenuItems(): React.ReactNode {
+        const { activeEntryIndex, entries, onScrollToEntry, readEntryIndex } = this.props;
+        const { aheadExpanded, behindExpanded } = this.state;
+        const { start, end } = getSliceRange(0, entries.length, activeEntryIndex >= 0 ? activeEntryIndex : 0, 5);
+
+        const items = [];
+
+        for (
+            let i = behindExpanded ? 0 : start,
+                l = aheadExpanded ? entries.length : end;
+            i < l;
+            i++
+        ) {
+            const entry = entries[i]!;
+
+            const icon = i == activeEntryIndex
+                ? <i className="icon icon-16 icon-dot u-text-negative" />
+                : entry.markedAsRead
+                ? <i className="icon icon-16 icon-dot u-text-muted" />
+                : i <= readEntryIndex
+                ? <i className="icon icon-16 icon-dot u-text-positive" />
+                : null;
+
+            items.push(
+                <MenuItem
+                    icon={icon}
+                    key={entry.entryId}
+                    onSelect={onScrollToEntry}
+                    primaryText={entry.title}
+                    value={i} />
+            );
+        }
+
+        return (
+            <>
+                {!behindExpanded && start !== 0 &&
+                    <a className="menu-item" href="#" onClick={this._handleExpandBehind}>
+                        <strong className="menu-item-primary-text u-text-muted">{start} entries hidden</strong>
+                    </a>}
+                {items}
+                {!aheadExpanded && end !== entries.length &&
+                    <a className="menu-item" href="#" onClick={this._handleExpandAhead}>
+                        <strong className="menu-item-primary-text u-text-muted">{entries.length - end} entries hidden</strong>
+                    </a>}
+            </>
+        );
+    }
+
+    private _handleClose = (): void => {
+        this.setState({
+            behindExpanded: false,
+            aheadExpanded: false
+        });
+    };
+
+    private _handleExpandBehind = (event: React.MouseEvent<any>): void => {
+        event.preventDefault();
+
+        this.setState({
+            behindExpanded: true
+        });
+    }
+
+    private _handleExpandAhead = (event: React.MouseEvent<any>): void => {
+        event.preventDefault();
+
+        this.setState({
+            aheadExpanded: true
+        });
+    }
+
+    private _handleOpenMarkAllAsReadModal = (): void => {
+        this.setState({
+            isMarkingAllAsRead: true
+        });
+    }
+
+    private _handleCloseMarkAllAsReadModal = (): void => {
+        this.setState({
+            isMarkingAllAsRead: false
+        });
+    }
+}
+
+function getSliceRange(start: number, end: number, position: number, size: number): { start: number, end: number } {
+    const behindSpace = position - start;
+    const aheadSpace = end - 1 - position;
+
+    if (behindSpace < size) {
+        return {
+            start: position - behindSpace,
+            end: Math.min(position + size + (size - behindSpace) + 1, end),
+        };
+    } else if (aheadSpace < size) {
+        return {
+            start: Math.max(position - size - (size - (aheadSpace)), start),
+            end: position + aheadSpace + 1,
+        };
+    } else {
+        return {
+            start: position - size,
+            end: position + size + 1
+        };
     }
 }
