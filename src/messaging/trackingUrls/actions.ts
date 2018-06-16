13df78c635db8c1fcc7fbeccc048ae770887173c
Shota Nozaki <emonkak@gmail.com>
2018-06-16 18:01:16 +0900

    Remove the tracking url pattern notification

--- src/messaging/trackingUrls/actions.ts
+++ src/messaging/trackingUrls/actions.ts
@@ -42,17 +42,10 @@ export function changeTrakingUrlCacheCapacity(capacity: number): Thunk {
     };
 }
 
-export function addTrackingUrlPattern(pattern: string): AsyncThunk {
-    return async ({ dispatch }) => {
-        dispatch({
-            type: 'TRACKING_URL_PATTERN_ADDED',
-            pattern
-        });
-
-        dispatch(sendNotification(
-            'Tracking URL pattern added',
-            'positive'
-        ));
+export function addTrackingUrlPattern(pattern: string): Event {
+    return {
+        type: 'TRACKING_URL_PATTERN_ADDED',
+        pattern
     };
 }
 
