a0a60b55ab07ced9e90915b94c564453fcc2666d
Shota Nozaki <emonkak@gmail.com>
2024-01-01 01:31:29 +0900

    fix(utils.throttle): always execute the callback at the last timing

--- packages/feedpon-utils/src/throttle.ts
+++ packages/feedpon-utils/src/throttle.ts
@@ -2,12 +2,25 @@ export default function throttle<TFunc extends (...args: any[]) => void>(
   func: TFunc,
   timeFrame: number,
 ): (this: ThisType<TFunc>, ...args: Parameters<TFunc>) => void {
-  let lastTime = 0;
+  let tailTimer: ReturnType<typeof setTimeout> = 0;
+  let lastInvoked = 0;
+
   return function (...args) {
+    if (tailTimer > 0) {
+      return;
+    }
+
     const now = Date.now();
-    if (now - lastTime >= timeFrame) {
+
+    if (now - lastInvoked >= timeFrame) {
       func.call(this, args);
-      lastTime = now;
+      lastInvoked = now;
+    } else {
+      tailTimer = setTimeout(() => {
+        func.call(this, args);
+        lastInvoked = Date.now();
+        tailTimer = 0;
+      }, timeFrame);
     }
   };
 }
