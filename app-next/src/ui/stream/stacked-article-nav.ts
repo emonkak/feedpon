5d26be2b167599e67d0ddf9de908125e109e9b63
Shota Nozaki <emonkak@gmail.com>
2026-09-12 14:01:51 +0900

    fix(next.ui.stream): scroll to current article top when scrolled past anchor

--- app-next/src/ui/stream/stacked-article-nav.ts
+++ app-next/src/ui/stream/stacked-article-nav.ts
@@ -5,10 +5,15 @@ export interface StackedArticleNavProps {}
 export const StackedArticleNav = createComponent(
   function StackedArticleNav({}: StackedArticleNavProps) {
     const scrollPrevious = () => {
-      window.scrollTo({
-        left: 0,
-        top: 0,
-      });
+      const anchor = document.querySelector('.StackScroller-Anchor');
+      if (anchor !== null && anchor.getBoundingClientRect().top <= -1) {
+        anchor.scrollIntoView();
+      } else {
+        window.scrollTo({
+          left: 0,
+          top: 0,
+        });
+      }
     };
     const scrollNext = () => {
       window.scrollTo({
@@ -18,80 +23,80 @@ export const StackedArticleNav = createComponent(
     };
 
     return html`
-    <nav class="StackedArticleNav" role="toolbar">
-      <button
-        aria-title="Previous article"
-        class="StackedArticleNav-Button"
-        title="Previous article"
-        type="button"
-        @click=${scrollPrevious}
-      >
-        <div aria-hidden="true" class="StackedArticleNav-Button-icon EmojiIcon" data-size="16">
-          <div class="EmojiIcon-glyph">🔼</div>
-        </div>
-      </button>
-      <button
-        aria-title="Next article"
-        class="StackedArticleNav-Button"
-        title="Next article"
-        type="button"
-        @click=${scrollNext}
-      >
-        <div aria-hidden="true" class="StackedArticleNav-Button-icon EmojiIcon" data-size="16">
-          <div class="EmojiIcon-glyph">🔽</div>
-        </div>
-      </button>
-      <button
-        aria-title="Extract full content"
-        class="StackedArticleNav-Button"
-        title="Extract full content"
-        type="button"
-      >
-        <div aria-hidden="true" class="StackedArticleNav-Button-icon EmojiIcon" data-size="16">
-          <div class="EmojiIcon-glyph">📃</div>
-        </div>
-      </button>
-      <button
-        aria-title="View original"
-        class="StackedArticleNav-Button"
-        title="View original"
-        type="button"
-      >
-        <div aria-hidden="true" class="StackedArticleNav-Button-icon EmojiIcon" data-size="16">
-          <div class="EmojiIcon-glyph">🌏︎</div>
-        </div>
-      </button>
-      <button
-        aria-title="Copy URL"
-        class="StackedArticleNav-Button"
-        title="Copy URL"
-        type="button"
-      >
-        <div aria-hidden="true" class="StackedArticleNav-Button-icon EmojiIcon" data-size="16">
-          <div class="EmojiIcon-glyph">🔗</div>
-        </div>
-      </button>
-      <button
-        aria-title="Translate article"
-        class="StackedArticleNav-Button"
-        title="Translate article"
-        type="button"
-      >
-        <div aria-hidden="true" class="StackedArticleNav-Button-icon EmojiIcon" data-size="16">
-          <div class="EmojiIcon-glyph">🔠</div>
-        </div>
-      </button>
-      <button
-        aria-title="Show discussions"
-        class="StackedArticleNav-Button"
-        title="Show discussions"
-        type="button"
-      >
-        <div aria-hidden="true" class="StackedArticleNav-Button-icon EmojiIcon" data-size="16">
-          <div class="EmojiIcon-glyph">💬</div>
-        </div>
-      </button>
-    </nav>
-  `;
+      <nav class="StackedArticleNav" role="toolbar">
+        <button
+          aria-title="Previous article"
+          class="StackedArticleNav-Button"
+          title="Previous article"
+          type="button"
+          @click=${scrollPrevious}
+        >
+          <div aria-hidden="true" class="StackedArticleNav-Button-icon EmojiIcon" data-size="16">
+            <div class="EmojiIcon-glyph">🔼</div>
+          </div>
+        </button>
+        <button
+          aria-title="Next article"
+          class="StackedArticleNav-Button"
+          title="Next article"
+          type="button"
+          @click=${scrollNext}
+        >
+          <div aria-hidden="true" class="StackedArticleNav-Button-icon EmojiIcon" data-size="16">
+            <div class="EmojiIcon-glyph">🔽</div>
+          </div>
+        </button>
+        <button
+          aria-title="Extract full content"
+          class="StackedArticleNav-Button"
+          title="Extract full content"
+          type="button"
+        >
+          <div aria-hidden="true" class="StackedArticleNav-Button-icon EmojiIcon" data-size="16">
+            <div class="EmojiIcon-glyph">📃</div>
+          </div>
+        </button>
+        <button
+          aria-title="View original"
+          class="StackedArticleNav-Button"
+          title="View original"
+          type="button"
+        >
+          <div aria-hidden="true" class="StackedArticleNav-Button-icon EmojiIcon" data-size="16">
+            <div class="EmojiIcon-glyph">🌏︎</div>
+          </div>
+        </button>
+        <button
+          aria-title="Copy URL"
+          class="StackedArticleNav-Button"
+          title="Copy URL"
+          type="button"
+        >
+          <div aria-hidden="true" class="StackedArticleNav-Button-icon EmojiIcon" data-size="16">
+            <div class="EmojiIcon-glyph">🔗</div>
+          </div>
+        </button>
+        <button
+          aria-title="Translate article"
+          class="StackedArticleNav-Button"
+          title="Translate article"
+          type="button"
+        >
+          <div aria-hidden="true" class="StackedArticleNav-Button-icon EmojiIcon" data-size="16">
+            <div class="EmojiIcon-glyph">🔠</div>
+          </div>
+        </button>
+        <button
+          aria-title="Show discussions"
+          class="StackedArticleNav-Button"
+          title="Show discussions"
+          type="button"
+        >
+          <div aria-hidden="true" class="StackedArticleNav-Button-icon EmojiIcon" data-size="16">
+            <div class="EmojiIcon-glyph">💬</div>
+          </div>
+        </button>
+      </nav>
+    `;
   },
 );
